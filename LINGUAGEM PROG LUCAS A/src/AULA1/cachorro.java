6ef51556543eff951f57a519107f8f35a29b4800
LucasAlvesG <117421570+LucasAlvesG@users.noreply.github.com>
2023-02-03 22:09:36 -0300

    Add files via upload

---
+++ LINGUAGEM PROG LUCAS A/src/AULA1/cachorro.java
@@ -0,0 +1,13 @@
+package AULA1;
+
+public class cachorro {
+String raca;
+
+public static void emitirSom () {
+	System.out.println("au");
+  }
+public void correr() {
+	System.out.println("correr");
+}
+}
+ 					
